331a6d61a3b391c00e0660e8eb7dd68e6142db65
wangmaolin <wangmaolin12@h-partners.com>
2024-12-17 14:59:38 +0800

    fix: 添加oat、readme 等合规文件,添加文件版权头

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
 import { randomBytes as randomBytesAndroidIos, seedSJCL } from 'react-native-randombytes';
 import { Platform } from 'react-native';
 import { randomBytes as randomBytesHarmony } from './index.harmony';
